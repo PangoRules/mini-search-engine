b99f07ab4f08385d19ff12564d36ee69b5ca6c37
Rafael Bolaños <animasdelmundo2@gmail.com>
2026-03-08 10:14:58 -0500

    feat: document_tokens implementation

--- src/mini_search/index_builder.py
+++ src/mini_search/index_builder.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from mini_search.models import Document
 from mini_search.storage.connection import get_connection
 from mini_search.storage.documents import fetch_all_documents
+from mini_search.storage.document_tokens import insert_idx_entries
 from mini_search.tokenizer import tokenize
 
 
@@ -19,6 +20,12 @@ def build_inverted_index(documents: list[Document]):
     return invertedIndex
 
 
+def main():
+    with get_connection() as conn:
+        documents = fetch_all_documents(conn)
+        indexes = build_inverted_index(documents)
+        insert_idx_entries(conn, indexes)
+
+
 if __name__ == "__main__":
-    test = fetch_all_documents(get_connection())
-    build_inverted_index(test)
+    main()
--- src/mini_search/storage/document_tokens.py
+++ src/mini_search/storage/document_tokens.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+from mini_search.storage.connection import get_connection
+
 
 def create_document_tokens_table(conn: sqlite3.Connection) -> None:
     conn.execute("""
@@ -10,3 +12,48 @@ def create_document_tokens_table(conn: sqlite3.Connection) -> None:
          )
      """)
     conn.commit()
+
+
+def insert_idx_entries(
+    conn: sqlite3.Connection, tokenDict: dict[str, set[int]]
+) -> None:
+    rows = []
+    for token, doc_ids in tokenDict.items():
+        for doc_id in doc_ids:
+            rows.append((doc_id, token))
+    try:
+        conn.executemany(
+            """
+            INSERT INTO document_tokens (doc_id, token)
+            VALUES (?, ?)
+        """,
+            rows,
+        )
+        conn.commit()
+    except sqlite3.IntegrityError:
+        print("Some of the relations already exist.")
+
+
+def fetch_idx_entries(conn: sqlite3.Connection, token: str) -> list[int]:
+    cursor = conn.execute(
+        """
+            SELECT doc_id, token
+            FROM document_tokens
+            WHERE token=?
+        """,
+        (token,),
+    )
+    rawResults = cursor.fetchall()
+
+    results = [rawResult["doc_id"] for rawResult in rawResults]
+
+    return results
+
+
+def main():
+    with get_connection() as conn:
+        print(fetch_idx_entries(conn, "python"))
+
+
+if __name__ == "__main__":
+    main()
